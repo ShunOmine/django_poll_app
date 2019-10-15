b95138b04ff8a1dcbd86545f86a60197f7e52532
ShunOmine <shun12baseball@gmail.com>
2019-10-15 12:27:49 +0900

    fix

--- polls/views.py
+++ polls/views.py
@@ -12,7 +12,7 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         """ Return the last five published questions. """
         return Question.objects.filter(
-            pub_date_lte=timezone.now()).order_by('-pub_date')[:5]
+            pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
@@ -20,7 +20,7 @@ class DetailView(generic.DetailView):
 
     def get_queryset(self):
         # Excludes any questions that aren't published yet.
-        return Question.objects.filter(pub_date_lte=timezone.now())
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
